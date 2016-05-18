b9ad1e486700652480ed1424e9463fca61aff901
Roman <rboyarov91@gmail.com>
2016-05-18 14:31:48 -0400

    create js variables for sites to open

--- js/button.js
+++ js/button.js
@@ -1,19 +1,38 @@
+var workSites = [
+	"https://perfectsense.atlassian.net/issues/?filter=-1"
+	];
+
+var opsSites = [
+	"https://zabbix.psdops.com/dashboard.php",
+	"https://beam-enterprise.psdops.com"
+	];
+
+var playSites = [
+	"http://lifehacker.com/",
+	"https://www.buzzfeed.com/",
+	"http://deals.kinja.com/"
+	];
+
 $(document).ready(function(){
 $('a.workLink').click(function(e) {
     console.log("hit the work button");	
     e.preventDefault();
-    window.open('https://perfectsense.atlassian.net/issues/?filter=-1');
+    for (w in workSites) {
+    	window.open(wordSites[w]);
+    }
 });
 
 $('a.opsLink').click(function(e) {
     e.preventDefault();
-    window.open('https://zabbix.psdops.com/dashboard.php');
+    for (o in opsSites) {
+    	window.open(opsSites[o]);
+    }
 });
 $('a.playLink').click(function(e) {
     console.log("hit the play button");
     e.preventDefault();
-    window.open('http://lifehacker.com/');
-    window.open('https://www.buzzfeed.com/');
-    window.open('http://deals.kinja.com/');
+    for (p in playSites) {
+    	window.open(playSites[p]);
+    }
 });
 });
