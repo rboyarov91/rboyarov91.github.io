340537c0a2434863e0d21d31d80017ce091b0852
Roman <rboyarov91@gmail.com>
2016-05-19 17:45:25 -0400

    add fade in and out of menu under main

--- js/button.js
+++ js/button.js
@@ -35,4 +35,49 @@ $('a.playLink').click(function(e) {
     	window.open(playSites[p]);
     }
 });
+$('a.link').mouseenter(function () {
+    console.log("mouse enter");
+    $(this).data('timeout', setTimeout(function () {
+        setSubVisible();
+    }, 500));
+}).mouseleave(function () {
+    clearTimeout($(this).data('timeout'));
+    $(this).data('timeout', setTimeout(function () {
+        setSubInVisible();
+    }, 500));
 });
+createSubmenu();
+});
+function setSubInVisible() {
+	console.log("set to invisible");
+	$("ul.submenu").fadeOut(1000);
+}
+function setSubVisible() {
+	console.log("set to visible");
+	$("ul.submenu").fadeIn(1000);
+}
+
+function createSubmenu() {
+	var getLocation = function(href) {
+    		var l = document.createElement("a");
+    		l.href = href;
+    		return l;
+		};
+	for (p in playSites) {
+	var a = $("<a />", {
+		class : "fa fa-lg fa-circle",
+		href : playSites[p],
+		target : "_blank"
+	});
+	li = $("<li />", {
+		class: "sub"
+	});
+	var l = getLocation(playSites[p]);
+	var span = $("<span />", {
+		text : l.hostname
+	});
+	li.append(a);
+	//li.append(span);
+	$('ul.submenu').append(li);
+	}
+}
